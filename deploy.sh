af182cae59fc0dd1ec40ab7775d1d3084ce20649
Vladislav Kazantsev <kazanc3v.v@yandex.ru>
2026-01-10 16:33:00 +0300

    Prepare deployment flow

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+# npm run docker:build
+ssh tw "rm -rf ~/network-area"
+ssh tw "mkdir -p ~/network-area"
+scp docker-compose.production.yml tw:~/network-area/docker-compose.yml
+scp network-area-images.tar .env tw:~/network-area/
+scp -r api/seeds tw:~/network-area/seeds
+ssh tw "cd ~/network-area && docker load -i network-area-images.tar && docker compose up -d"
